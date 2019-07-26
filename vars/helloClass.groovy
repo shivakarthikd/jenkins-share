a3e3f42320257335407681f999283f0d9bf61641
shivakarthikd <52204995+shivakarthikd@users.noreply.github.com>
2019-07-26 18:06:49 +0000

    Update helloClass.groovy

--- vars/helloClass.groovy
+++ vars/helloClass.groovy
@@ -2,7 +2,7 @@ import org.share.bar
 def call(message){
     b=bar.newInstance()
     if(message == "TRUE"){
-        echo "This is class hello ${b.local} , ${message}"
+        echo "This is class hello ${b.Local} , ${message}"
     }
 
 }
