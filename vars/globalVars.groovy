f15f3ad5a4bdf22f7699ced8377432917946400e
shivakarthikd <52204995+shivakarthikd@users.noreply.github.com>
2019-07-26 08:11:23 +0000

    Update globalVars.groovy

--- vars/globalVars.groovy
+++ vars/globalVars.groovy
@@ -2,13 +2,13 @@ import org.share.bar
 def call(Map args){
     
 }
-def info(message) {
+def info(message=args.v1) {
     if(args.v1=='TRUE') {
         echo "INFO: ${message}"
     }
 }
 
-def warn(message) {
+def warn(message=args.v2) {
     if(args.v2=="FALSE") {
         echo "WARNING: ${message}"
     }
